ac4b7a652c3897ad1bb3d49ff941b30bdd4046b0
Miles Grimes <12803415+TheCrimsonKing92@users.noreply.github.com>
2024-02-23 18:56:28 -0600

    Reconfigure generator probabilities

--- media/lua/shared/PGGeneratorUtils.lua
+++ media/lua/shared/PGGeneratorUtils.lua
@@ -24,9 +24,9 @@ generatorUtils.GENERATOR_TYPES = {
 local GENERATOR_TYPES = generatorUtils.GENERATOR_TYPES
 
 generatorUtils.GENERATOR_PROBABILITIES = {
-    Gas = 5 / 10,
-    Propane = 4 / 10,
-    DualFuel = 1 / 10
+    Gas = 7 / 20,
+    Propane = 9 / 20,
+    DualFuel = 4 / 20
 }
 
 local GENERATOR_PROBABILITIES = generatorUtils.GENERATOR_PROBABILITIES
